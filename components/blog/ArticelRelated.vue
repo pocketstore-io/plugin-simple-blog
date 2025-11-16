980ec303be5ec611aaf12df71483b585ef9e2890
jonathan-martz <github@jmartz.de>
2025-11-16 20:04:16 +0100

    remove dep from plugin

--- components/blog/ArticelRelated.vue
+++ components/blog/ArticelRelated.vue
@@ -1,8 +1,10 @@
 <template>
   <section class="grid grid-cols-6 gap-3 mt-3">
+    <!--
     <div v-for="item in items" class="col-span-6 md:col-span-2">
       <PluginCard :identifier="item.id" />
     </div>
+    -->
   </section>
 </template>
 <script setup lang="ts">
@@ -14,7 +16,7 @@ const items = ref([]);
 const pb = usePocketBase();
 
 const load = async () =>{
-  items.value = (await pb.collection('plugins').getList(1,3,{})).items;
+  // items.value = (await pb.collection('plugins').getList(1,3,{})).items;
 }
 onMounted(load)
 </script>
